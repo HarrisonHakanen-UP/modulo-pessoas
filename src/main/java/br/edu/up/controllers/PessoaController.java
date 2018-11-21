7c54fd931c9bb24e218835d9c2495a014ff0cec7
Sistemas Corporativos <harrisonhakinen@gmail.com>
2018-11-21 02:53:28 -0200

    Adicionado método e rota para atualizar uma pessoa

--- src/main/java/br/edu/up/controllers/PessoaController.java
+++ src/main/java/br/edu/up/controllers/PessoaController.java
@@ -2,12 +2,16 @@ package br.edu.up.controllers;
 
 import java.util.Optional;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -44,6 +48,22 @@ public class PessoaController {
 	public void cadastrar(@RequestBody Pessoa pessoa) {
 		pessoaRepository.save(pessoa);
 		System.out.println("Pessoa cadastrada com sucesso!");
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Pessoa> atualizar(@PathVariable int id, @Valid @RequestBody Pessoa pessoa) {
+		
+		Optional<Pessoa> pessoaRepositorio = pessoaRepository.findById(id);
+		
+		if (pessoaRepositorio.get() != null) {
+			Pessoa dadosPessoa = pessoaRepositorio.get();
+			BeanUtils.copyProperties(pessoa, dadosPessoa);
+			pessoaRepository.save(dadosPessoa);
+			
+			System.out.println("Pessoa atualizada com sucesso!");
+		}
+		
+		return ResponseEntity.notFound().build();
 	}
 	
 	@DeleteMapping("/{id}")
